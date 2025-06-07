5b60b308e65d3256ed6805f4f425db8efeb0a6c7
Dinmuhammad Yagafarov <95093475+Yagafarov@users.noreply.github.com>
2025-06-07 09:20:32 +0000

    til to'g'irlandi

--- src/components/WordCard.jsx
+++ src/components/WordCard.jsx
@@ -10,7 +10,9 @@ const flags = {
 
 const WordCard = ({ word, translation, image, definition, lang }) => {
   const speak = () => {
-    const utterance = new SpeechSynthesisUtterance(translation + " — это "+definition);
+    const utterance = new SpeechSynthesisUtterance(
+    lang === 'uz' ? `${translation} — это ${definition}` : word
+  );
     utterance.lang = lang; // Tilda talaffuz qilish
     window.speechSynthesis.speak(utterance);
   };
